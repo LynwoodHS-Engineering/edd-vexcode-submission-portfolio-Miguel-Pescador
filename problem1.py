3e1d6b51324902d2ec797c4654f77cf0b4472b2c
Miguel Pescador <migpes2806@stu.mylusd.org>
2025-01-21 12:27:32 -0800

    Update problem1.py

--- problem1.py
+++ problem1.py
@@ -36,37 +36,6 @@ wait(200, MSEC)
 print("\033[2J")
 
 #endregion VEXcode Generated Robot Configuration
-from vex import *
-import urandom
-
-# Brain should be defined by default
-brain = Brain()
-
-# Robot configuration code
-optical_1 = Optical(Ports.PORT1)
-motor_2 = Motor(Ports.PORT2, GearSetting.RATIO_18_1, False)
-
-# Wait for sensor to initialize
-wait(30, MSEC)
-
-# Make random actually random
-def initializeRandomSeed():
-    wait(100, MSEC)
-    random = brain.battery.voltage(MV) + brain.battery.current(CurrentUnits.AMP) * 100 + brain.timer.system_high_res()
-    urandom.seed(int(random))
-
-# Set random seed
-initializeRandomSeed()
-
-# Helper to make playing sounds from the V5 in VEXcode easier
-def play_vexcode_sound(sound_name):
-    print("VEXPlaySound:" + sound_name)
-    wait(5, MSEC)
-
-# Add a small delay to make sure we don't print in the middle of the REPL header
-wait(200, MSEC)
-# Clear the console to make sure we don't have the REPL in the console
-print("\033[2J")
 
 def when_started1():
     while True:
